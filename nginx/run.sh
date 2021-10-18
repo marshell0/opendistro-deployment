1aa1d67e6d4cfb675c914c7000d0dfa36f26113a
marshell0 <36255800+marshell0@users.noreply.github.com>
2021-10-18 17:14:38 +0800

    Delete run.sh

--- nginx/run.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-    --restart=unless-stopped \
-    --name nginx \
-    --mount type=bind,source=/home/elasticsearch/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
-    --network host \
-    -e 'NGINX_PORT=8043' \
-    nexus.ubssdic.com:8445/nginx:1.21
