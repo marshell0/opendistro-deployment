9904dcb169d1ea2d4e39a52072fe64475b16c170
marshell0 <36255800+marshell0@users.noreply.github.com>
2021-10-18 17:16:22 +0800

    Delete logstash_exporter.sh

--- script/logstash_exporter.sh
+++
@@ -1,8 +0,0 @@
- docker run -d \
-  --restart=unless-stopped \
-  --name=logstash-exporter \
-  --hostname=logstash-exporter \
-  --network host \
-  nexus.ubssdic.com:8445/alxrem/prometheus-logstash-exporter:latest \
-  -logstash.host localhost \
-  -logstash.port 9610
